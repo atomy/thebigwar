59c0b2b887423aaaf7cf43f6f09061dfc516c319
atomy <atomy@bd1e61f2-b670-413e-9183-6a7b7d94a23c>
2009-06-15 14:04:47 +0000

    +impressum

--- impressum.php
+++ impressum.php
@@ -8,7 +8,8 @@
 <fieldset><legend>Spielbetreiber</legend>
 <ul>
 	<li>Projektleader</li><br><br>
-	<li>P. Mueller alias LXUSER</li>
+	<li>atomy</li>
+	<li>xsawa</li>
 </ul>
 <ul>
 <li><em xml:lang="en">The Big War</em> ist ein privates Projekt, das zur Zeit keinerlei kommerzielle Absichten verfolgt.</li>
@@ -21,12 +22,11 @@
 <p>Sollten Sie eine Frage oder Anmerkung bezüglich des Spiels haben, haben Sie folgende Möglichkeiten:</p>
 <ul>
 	<li>Setzen Sie sich per <a href="mailto:atomy@jackinpoint.net">eMail</a> mit dem Betreiber in Verbindung.</li>
-	<li>Stellen Sie ihre Frage öffentlich im <a href=http://www.stephanlinden.net/forum/" xml:lang="en">Forum</a>.</li>
+	<li>Stellen Sie ihre Frage öffentlich im <a href="http://www.stephanlinden.net/forum/" xml:lang="en">Forum</a>.</li>
 	<li>Fragen Sie im Support-Kanal des <a href="http://<?=$_SERVER['HTTP_HOST'].h_root?>/chat.php">Chats</a> nach.</li>
 
 </ul>
 <p>Es besteht keine Garantie auf eine Antwort, normalerweise erfolgt eine solche aber innerhalb eines Tages.</p>
-<p><strong>Fragen zum Spiel über andere Wege als die hier agegebenen, wie zum Beispiel über die private <acronym title="Ring Ring Ring" xml:lang="en">Telefon-Nr.</acronym> oder <acronym title="I seek you" xml:lang="en">ICQ-Nr.</acronym> des Betreibers, werden als unverschämt erachtet und ignoriert!</strong></p>
 </fieldset>
 
 <p></p>
@@ -34,21 +34,11 @@
 <fieldset><legend>Credits</legend>
 <ul>
 		
-		<li>Lxuser - TBW-Coder, Sysadmin & Projektleiter</li>
-		<li>Zotulus - TBW-Coder</li>
-		<li>Candid Dauth - SUA Coder</li>
+		<li>Das Spiel basier auf den Programmcode von TBW bzw. SUA</li>
 </ul>
 </fieldset>
 
 <p></p>
-
-<fieldset><legend>Grafik</legend>
-<ul>
-    <li>Bernd Unger (<a href="http://www.unger.3d-operators.de" target="_blank">http://www.unger.3d-operators.de</a>)</li>
-	<li>3D-DESIGNER & MEDIENGESTALTER</li>
-	<li>Karolingerstr. 1</li>
-	<li>40223 Düsseldorf</li>
-</ul>
 </fieldset>
 <p></p><p></p>
 <?php
