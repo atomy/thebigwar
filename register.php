7e9822e04b34ca7ff0985e2c49b77adff82e5d45
atomy <atomy@bd1e61f2-b670-413e-9183-6a7b7d94a23c>
2009-09-11 00:42:23 +0000

    lets open stuff in a new window

--- register.php
+++ register.php
@@ -174,7 +174,7 @@
 			<dt><label for="hauptplanet">Gewünschter Name des Hauptplaneten*</label></dt>
 			<dd><input type="text" id="hauptplanet" name="hauptplanet"<?=isset($_POST['hauptplanet']) ? ' value="'.utf8_htmlentities($_POST['hauptplanet']).'"' : ''?> maxlength="24" /></dd>
 		</dl>
-		<div><input type="checkbox" class="checkbox" name="nutzungsbedingungen" id="nutzungsbedingungen" /> <label for="nutzungsbedingungen">Ich habe die <a href="http://wiki.thebigwar.org/index.php/Regelwerk">Nutzungsbedingungen / Regeln</a> gelesen und akzeptiere sie.</label></div>
+		<div><input type="checkbox" class="checkbox" name="nutzungsbedingungen" id="nutzungsbedingungen" /> <label for="nutzungsbedingungen">Ich habe die <a href="http://wiki.thebigwar.org/index.php/Regelwerk" target="_blank">Nutzungsbedingungen / Regeln</a> gelesen und akzeptiere sie.</label></div>
 		<ul>
 			<li><button type="submit">Registrieren</button></li>
 		</ul>
