76ebc536bdd378c5a4d087bee2091dc37087d3a6
atomy <atomy@bd1e61f2-b670-413e-9183-6a7b7d94a23c>
2009-10-31 03:55:41 +0000

    moar default stuff

---
+++ default/include/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+        define( "GLOBAL_GAMEURL", "http://testuni.thebigwar.org/" );
+        define( "GLOBAL_SGOACCNAME", "admin" );
+        define( "GLOBAL_DEMOACCNAME", "demo" );
+        define( "GLOBAL_DEMOACCPASS", "demo" );
+
+        if ( !defined( "TBW_ROOT" ) )
+                define( "TBW_ROOT", "/var/www/tbw-test.jackinpoint.net/htdocs/" );
+
+        date_default_timezone_set( "Europe/Berlin" );
+?>
