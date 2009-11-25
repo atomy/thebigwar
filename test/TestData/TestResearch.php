9f748f2c4c0c00d3742efeabf863e5f6b1675ce8
atomy <atomy@bd1e61f2-b670-413e-9183-6a7b7d94a23c>
2009-11-25 16:33:26 +0000

    missed one file

---
+++ test/TestData/TestResearch.php
@@ -0,0 +1,59 @@
+<?php
+
+class TestResearch
+{
+	// id string of research
+	private $id;
+	
+	// is it global?
+	private $global;
+	
+	// used when research is global, specifies the planet where it was started from
+	private $startPlanet;
+	
+	public function __construct($id, $global)
+	{
+		$this->id = $id;
+		$this->global = $global;
+	}
+	
+	public function setStartPlanet($set)
+	{
+		$this->startPlanet = $set;
+	}
+	
+	public function getStartPlanet()
+	{
+		return $this->startPlanet;
+	}
+	
+	public function setGlobal($set)
+	{
+		$this->global = $set;
+	}
+	
+	public function getGlobal()
+	{
+		return $this->global;
+	}
+	
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+	
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function isGlobal()
+	{
+		if ( $this->global )
+			return true;
+		else
+			return false;
+	}
+}
+
+?>
