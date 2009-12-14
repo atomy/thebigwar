2cf499926440a684f600e78c241003b797546f96
atomy <atomy@bd1e61f2-b670-413e-9183-6a7b7d94a23c>
2009-12-14 16:54:54 +0000

    reformatted and got rid of warnings of empty variables

--- db_things/count_system.php
+++ db_things/count_system.php
@@ -1,114 +1,113 @@
 #!/usr/bin/php
 
 <?php
-        $USE_OB = false;
-        require_once '../include/config_inc.php';
-        require TBW_ROOT.'/engine/include.php';
+$USE_OB = false;
+require_once '../include/config_inc.php';
+require TBW_ROOT . '/engine/include.php';
 
-        if(!isset($_SERVER['argv'][1]))
-        {
-                fputs(STDERR, "Usage: ".$_SERVER['argv'][0]." <Database ID>\n");
-                exit(1);
-        }
-        else
-        {
-                $databases = get_databases();
-                if(!define_globals($_SERVER['argv'][1]))
-                {
-                        fputs(STDERR, "Unknown database.\n");
-                        exit(1);
-                }
-        }
+if ( ! isset( $_SERVER['argv'][1] ) )
+{
+    fputs( STDERR, "Usage: " . $_SERVER['argv'][0] . " <Database ID>\n" );
+    exit( 1 );
+}
+else
+{
+    $databases = get_databases();
+    if ( ! define_globals( $_SERVER['argv'][1] ) )
+    {
+        fputs( STDERR, "Unknown database.\n" );
+        exit( 1 );
+    }
+}
 
-        $dh = opendir(global_setting("DB_PLAYERS"));
- 
-	
-	while(($filename = readdir($dh)) !== false)
-	{
-                         	if(!is_file(global_setting("DB_PLAYERS").'/'.$filename))
-	               	        continue;
+$dh = opendir( global_setting( "DB_PLAYERS" ) );
 
-                         	$user = Classes::User(urldecode($filename));
-				$planets = $user->getPlanetsList();
-                       
-			        foreach($planets as $planet)
-				{
-				$besiedelt += count($planet);
-				}
-				
-				$last_activity = $user->getLastActivity();
-				if($last_activity > (time()-259200)) $activ += 1;
-				
-	
-	}
-	
+$besiedelt = 0;
+$activ = 0;
+$anzahl = 0;
+$anzahl1 = 0;
+
+while ( ( $filename = readdir( $dh ) ) !== false )
+{
+    if ( ! is_file( global_setting( "DB_PLAYERS" ) . '/' . $filename ) )
+        continue;
+    
+    $user = Classes::User( urldecode( $filename ) );
+    $planets = $user->getPlanetsList();
+    
+    foreach ( $planets as $planet )
+    {
+        $besiedelt += count( $planet );
+    }
+    
+    $last_activity = $user->getLastActivity();
+    if ( $last_activity > ( time() - 259200 ) )
+        $activ += 1;
+
+}
+
+echo "Besiedelte Planeten: ", $besiedelt, "\n\n";
+echo "Aktive User innerhalb der letzten 3 Tage: ", $activ, "\n\n";
+
+$galaxy_n = 1;
+$system_n = 1;
+
+__autoload( 'Galaxy' );
+$galaxy_count = getGalaxiesCount();
+
+$galaxy = Classes::Galaxy( $galaxy_n );
+
+$next_system = $system_n + 1;
+if ( $next_system > 999 )
+    $next_system = 1;
+
+for ( $c = 1; $c <= 999; $c ++ )
+{
+    $planets_count = $galaxy->getPlanetsCount( $c );
+    {
+        for ( $i = 1; $i <= $planets_count; $i ++ )
+        {}
+        $anzahl += $planets_count;
+    }
+
+}
+echo "Es sind ", $anzahl, " Planetenpositionen in Galaxie 1 verfuegbar\n\n";
+
+#$prozent = $besiedelt * 100 / $anzahl;
 
 
-	
-	
-	echo "Besiedelte Planeten: ", $besiedelt, "\n\n";
-	echo "Aktive User innerhalb der letzten 3 Tage: ", $activ, "\n\n";
+#echo "Es sind ", round($prozent, 2), " Prozent belegt!\n"; 
 
 
-        $galaxy_n = 1;
-        $system_n = 1;
+$galaxy_n1 = 2;
+$system_n = 1;
 
-	 __autoload('Galaxy');
-        $galaxy_count = getGalaxiesCount();
+__autoload( 'Galaxy' );
+$galaxy_count = getGalaxiesCount();
 
-        $galaxy = Classes::Galaxy($galaxy_n);
-	
-	$next_system = $system_n+1;
-        if($next_system > 999)
-        $next_system = 1;
+$galaxy1 = Classes::Galaxy( $galaxy_n1 );
 
-	for($c=1; $c <= 999; $c++)
-	{		
-		$planets_count = $galaxy->getPlanetsCount($c);
-        	{	 
-			for($i=1; $i <= $planets_count; $i++)
-			{
-			}	
-			$anzahl += $planets_count;
-		}		
-	
-	}
-                        echo "Es sind ", $anzahl, " Planetenpositionen in Galaxie 1 verfuegbar\n\n";
-                        
-                        #$prozent = $besiedelt * 100 / $anzahl;
-                        
-                        #echo "Es sind ", round($prozent, 2), " Prozent belegt!\n"; 
+$next_system = $system_n + 1;
+if ( $next_system > 999 )
+    $next_system = 1;
 
-	$galaxy_n1 = 2;
-        $system_n = 1;
+for ( $c = 1; $c <= 999; $c ++ )
+{
+    $planets_count = $galaxy1->getPlanetsCount( $c );
+    {
+        for ( $i = 1; $i <= $planets_count; $i ++ )
+        {}
+        $anzahl1 += $planets_count;
+    }
 
-	 __autoload('Galaxy');
-        $galaxy_count = getGalaxiesCount();
+}
+$gesamtpp = $anzahl + $anzahl1;
+echo "Es sind ", $anzahl1, " Planetenpositionen in Galaxie 2 verfuegbar\n\n";
+echo "Es sind ", $gesamtpp, " Planetenpositionen gesamt verfuegbar\n\n";
 
-        $galaxy1 = Classes::Galaxy($galaxy_n1);
-	
-	$next_system = $system_n+1;
-        if($next_system > 999)
-        $next_system = 1;
+$prozent = $besiedelt * 100 / ( $gesamtpp );
 
-	for($c=1; $c <= 999; $c++)
-	{		
-		$planets_count = $galaxy1->getPlanetsCount($c);
-        	{	 
-			for($i=1; $i <= $planets_count; $i++)
-			{
-			}	
-			$anzahl1 += $planets_count;
-		}		
-	
-	}
-			$gesamtpp = $anzahl + $anzahl1;
-                        echo "Es sind ", $anzahl1, " Planetenpositionen in Galaxie 2 verfuegbar\n\n";
-			echo "Es sind ", $gesamtpp, " Planetenpositionen gesamt verfügbar\n\n";
-                        
-                        $prozent = $besiedelt * 100 / ($gesamtpp);
-                        
-                        echo "Es sind ", round($prozent, 2), " Prozent belegt!\n"; 
-	closedir($dh); 
+echo "Es sind ", round( $prozent, 2 ), " Prozent belegt!\n";
+closedir( $dh );
 ?>
 
