7c21cb30a94580f15bf3cdff285aca451dc55b07
atomy <atomy@bd1e61f2-b670-413e-9183-6a7b7d94a23c>
2010-01-16 18:35:51 +0000

    config updated

--- default/include/config_inc.php
+++ default/include/config_inc.php
@@ -4,10 +4,18 @@
     define("GLOBAL_DEMOACCNAME", "demo");
     define("GLOBAL_DEMOACCPASS", "demo");
 
-	// TODO
-  	//define("TBW_ROOT", "/var/www/tbw-test.jackinpoint.net/htdocs/");
+	define("TBW_ROOT", "/var/www/tbw-test.jackinpoint.net/htdocs/");
+	
+	// url for news iframe used at front page
+	define("TBW_EXT_NEWSURL", 'http://forum.thebigwar.org/ext/news.php');
 
 	require_once( TBW_ROOT."include/util.php" );
+
+	// eggdrop, announcing new user registrations into IRC
+    $bb2egg['botip'] = "";
+    $bb2egg['botport'] = ""; 
+    $bb2egg['pass'] = "";
+    $bb2egg['channel_tbw'] = "#tbw";
 
     date_default_timezone_set("Europe/Berlin");
 ?>
