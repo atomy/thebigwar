383f791f2875b98bdc4ab83fbb0bd41b4c039e58
atomy <atomy@bd1e61f2-b670-413e-9183-6a7b7d94a23c>
2010-01-25 21:08:04 +0000

    fzero als go entfernt

--- login/nachrichten.php
+++ login/nachrichten.php
@@ -581,7 +581,6 @@
 
 <ul class="nachrichten-neu-link">
     <li><a href="nachrichten.php?to=Stoffel&<?=htmlentities(session_name().'='.urlencode(session_id()))?>" accesskey="n" tabindex="1.3">Nachricht an <kbd>G</kbd>O (Stoffel)</a></li>
-    <li><a href="nachrichten.php?to=fzero&<?=htmlentities(session_name().'='.urlencode(session_id()))?>" accesskey="n" tabindex="1.4">Nachricht an <kbd>G</kbd>O (fzero)</a></li>
 </ul>
 
 <dl class="nachrichten-kategorien">
