18692bfcb22f0c99deb525b4d49c4267df034463
atomy <atomy@bd1e61f2-b670-413e-9183-6a7b7d94a23c>
2010-02-17 19:29:12 +0000

    missed files for the new site

---
+++ ueber_tbw.php
@@ -0,0 +1,43 @@
+<?php
+	require_once( 'include/config_inc.php' );
+	require( TBW_ROOT.'include.php' );
+
+    startseite_html_head();
+?>
+
+<div id="history">
+<h1>&Uuml;ber TheBigWar</h1>
+<p>
+TheBigWar ist ein Aufbau- und Strategiespiel welches im Weltraum spielt. So k&ouml;nnen in einem gemeinsamen Universum Planeten bev&ouml;lkert werden, Geb&auml;ude gebaut, geforscht und K&auml;mpfe zwischen den Planeten ausgefochten werden. Jeder Spieler ist der Herr &uuml;ber eine begrenzte Anzahl von Planeten mit deren Hilfe er sich eine Flotte aufbauen kann und somit neue Welten erforschen und in Kontakt mit anderen Spielern ger&auml;t. So kann der neue Kontakt in einem friedlichen und regen Handel von Rohstoffen einher gehen aber auch zu einem erbittertem Krieg f&uuml;hren, in welchem es um das Erk&auml;mpfen von Rohstoffen und Vernichten der gegnerischen Flotte geht.
+</p>
+<p>
+Das Projekt basiert auf dem Sourcecode vorangegangener Entwickler denen hier auch nochmal ein Dank ausgesprochen werden soll. Ein davon noch aktives Projekt findet sich unter <a href="http://www.s-u-a.net/">Stars Under Attack</a>.
+</p>
+
+<h2>Features:</h2>
+<ul>
+<li>11 Geb&auml;ude</li>
+<li>12 Forschungsm&ouml;glichkeiten</li>
+<li>6 verschiedene Roboter</li>
+<li>16 Raumschiffklassen</li>
+<li>7 Verteidigungsanlagen</li>
+<li>Handelsrechner</li>
+<li>Imperiums&uuml;bersicht</li>
+<li>Echtzeitstrategie</li>
+<li>Forschung l&auml;sst sich global oder lokal durchf&uuml;hren</li>
+<li>Ausgekl&uuml;geltes Allianzsystem</li>
+<li>Schlie&szlig;e B&uuml;ndnisse mit einzelnen Spielern</li>
+<li>Variabler Handelskurs, der sich den Zust&auml;nden im Universum anpasst</li>
+<li>Handelssystem: Gebe n&auml;hernden Transporten Rohstoffe mit auf den R&uuml;ckweg</li>
+<li>Komfortable Einstellungsm&ouml;glichkeiten, die das Spielen erleichtern</li> 
+<li>V&ouml;llige Ummodellierbarkeit des Designs durch Skins</li>
+<li>Stationiere Flotten bei Verb&uuml;ndeten, um diese zu unterst&uuml;tzen</li>
+<li>Fliege gemeinsame Angriffe mit deinen Verb&uuml;ndeten</li>
+<li>Sichern deine Ressourcen per Saveflugfunktion</li>
+<li>Ver&ouml;ffentliche Nachrichten einfach per Mausklick.</li>
+</ul>
+</div> <!-- History / -->
+
+<?php 
+    startseite_html_foot();
+?>
