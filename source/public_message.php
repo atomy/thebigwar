25aecc32caf30aa56322a05a5fae4d2e07bdacb7
atomy <atomy@bd1e61f2-b670-413e-9183-6a7b7d94a23c>
2011-01-14 13:16:27 +0000

    nothing, just increasing revision number

--- source/public_message.php
+++ source/public_message.php
@@ -9,7 +9,7 @@ require($_SERVER['DOCUMENT_ROOT'].'/include.php');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
-    <head>
+    <head> 
         <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
         <title xml:lang="en">TBW&ndash;The Big War</title>
 <?php
